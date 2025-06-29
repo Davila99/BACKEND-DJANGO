dcdbc51c05aeebfda781153996a12214086bb48c
eliseo <davilaeliseo453@gmail.com>
2025-06-29 09:41:48 -0600

    create de models

--- soportecontrol/registrolab/models.py
+++ soportecontrol/registrolab/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class RegistroLab(models.Model):
+    carrera = models.EmailField()
+    asignatura = models.CharField(max_length=100)
+    # horaEntrada = models.TimeField(auto_now_add=True)
+    # horaSalida = models.CharField(max_length=100)
+    docente = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.carrera} - {self.asignatura} - {self.docente} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
