4b44b69dd901de18c2d544d11560b7f548dbe196
eliseo <davilaeliseo453@gmail.com>
2025-06-30 17:31:44 -0600

    cambios en el modelo

--- soportecontrol/registrolab/models.py
+++ soportecontrol/registrolab/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Create your models here.
 class RegistroLab(models.Model):
-    carrera = models.EmailField()
+    carrera = models.CharField(max_length=100)
     asignatura = models.CharField(max_length=100)
-    # horaEntrada = models.TimeField(auto_now_add=True)
-    # horaSalida = models.CharField(max_length=100)
+    horaEntrada = models.TimeField(auto_now_add=True)
+    horaSalida = models.CharField(max_length=100)
     docente = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
-        return self.asignatura
+        return self.carrera + " - " + self.asignatura
