94211ec5c61c5578cbd595cbbf9fdd2e4957c090
eliseo <davilaeliseo453@gmail.com>
2025-07-03 11:56:55 -0600

    cambios

--- soportecontrol/registrolab/models.py
+++ soportecontrol/registrolab/models.py
@@ -16,16 +16,16 @@ class Carrera(models.Model):
     
 class Docente(models.Model):
     nombre = models.CharField(max_length=200)
-    asignatura_id = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
     def __str__(self):
         return self.nombre
     
 class RegistroLab(models.Model):
     fecha = models.DateField('Fecha')
+    asignatura_id = models.ForeignKey(Asignatura, on_delete=models.CASCADE)
     docentes_id = models.ForeignKey(Docente, on_delete=models.CASCADE)
     laboratorio_id = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
     carreras_id = models.ForeignKey(Carrera, on_delete=models.CASCADE)
     hora_inicio = models.TimeField('Hora Entrada')
     hora_fin = models.TimeField('Hora Salida')
     def __str__(self):
-         return self.fecha.strftime('%Y-%m-%d')
+         return self.fecha.strftime('%Y-%m-%d') + ' ' + self.hora_inicio.strftime('%H:%M') + ' - ' + self.hora_fin.strftime('%H:%M') + ' | ' + str(self.asignatura_id) + ' | ' + str(self.docentes_id) + ' | ' + str(self.laboratorio_id) + ' | ' + str(self.carreras_id)
